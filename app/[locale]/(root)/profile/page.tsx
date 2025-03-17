072b744d8ee0d2a743fc0835c6d7f5e11095b980
Ethan LE NEINDRE <ethan.le.neindre@sap.com>
2025-03-17 22:55:55 +0100

    feat: Add profile page

---
+++ app/[locale]/(root)/profile/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { MonoText, MonoTitle } from "@/components/MonoText";
+import { MainHeader } from "@/components/PageSkeleton";
+import { Metadata } from "next";
+import { useScopedI18n } from "@/locales/client";
+
+// export const metadata: Metadata = {
+//   title: "Projets - Ethan Le Neindre",
+//   description: `Voici mes projets actuels et futurs.`,
+//   robots: "index, follow",
+// };
+
+export default function Projects() {
+  const t = useScopedI18n("profile");
+  return (
+    <div className="flex flex-col gap-8">
+      <MainHeader>
+        <MonoText className="text-lg md:text-2xl max-w-96">{ t('description') }</MonoText>
+      </MainHeader>
+      <hr className="mx-4 border-black dark:border-white border rounded-full" />
+      <div>
+        <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.personal') }</MonoTitle>
+        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.personal') }</MonoText>
+      </div>
+      <hr className="mx-4 border-black dark:border-white border rounded-full" />
+      <div>
+        <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.scolarship') }</MonoTitle>
+        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.scolarship') }</MonoText>
+      </div>
+      <hr className="mx-4 border-black dark:border-white border rounded-full" />
+      <div>
+        <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.professional') }</MonoTitle>
+        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.professional') }</MonoText>
+      </div>
+    </div>
+  );
+}
