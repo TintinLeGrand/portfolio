641471d7972d4ce88341606b5349804e61aec295
Ethan LE NEINDRE <ethan.le.neindre@sap.com>
2025-03-17 23:14:41 +0100

    fix: Profile display

--- app/[locale]/(root)/profile/page.tsx
+++ app/[locale]/(root)/profile/page.tsx
@@ -16,22 +16,22 @@ export default function Projects() {
   return (
     <div className="flex flex-col gap-8">
       <MainHeader>
-        <MonoText className="text-lg md:text-2xl max-w-96">{ t('description') }</MonoText>
+        <MonoText className="text-lg md:text-2xl text-justify">{ t('description') }</MonoText>
       </MainHeader>
       <hr className="mx-4 border-black dark:border-white border rounded-full" />
       <div>
         <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.personal') }</MonoTitle>
-        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.personal') }</MonoText>
+        <MonoText className="text-lg md:text-2xl text-justify">{ t('texts.personal') }</MonoText>
       </div>
       <hr className="mx-4 border-black dark:border-white border rounded-full" />
       <div>
         <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.scolarship') }</MonoTitle>
-        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.scolarship') }</MonoText>
+        <MonoText className="text-lg md:text-2xl text-justify">{ t('texts.scolarship') }</MonoText>
       </div>
       <hr className="mx-4 border-black dark:border-white border rounded-full" />
       <div>
         <MonoTitle className="text-3xl md:text-5xl font-semibold">{ t('titles.professional') }</MonoTitle>
-        <MonoText className="text-lg md:text-2xl max-w-96">{ t('texts.professional') }</MonoText>
+        <MonoText className="text-lg md:text-2xl text-justify">{ t('texts.professional') }</MonoText>
       </div>
     </div>
   );
